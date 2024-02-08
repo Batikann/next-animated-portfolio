4ae501b5a5f79da5e764bb5046b1ad5354e726d5
Batikann <emiruar123@gmail.com>
2024-02-08 11:49:06 +0300

    Framer Motion Page Transition Added every page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,17 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
 const ContactPage = () => {
-  return <div>ContactPage</div>
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: '-200vh' }}
+      animate={{ y: '0%' }}
+      transition={{ duration: 1 }}
+    >
+      <div>ContactPage</div>
+    </motion.div>
+  )
 }
 export default ContactPage
